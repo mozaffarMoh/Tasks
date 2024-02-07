5ac475ed6bb59201ea60c931fea8bfb7116ce21a
Mozaffar <FERAWWWESS@gmail.com>
2024-02-07 15:18:36 +0300

    new era

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "out",
-  generateStaticPaths: async function () {
-    return {
-      // Define your static paths here
-      paths: {
-        "/": { page: "/" },
-        // Add more paths as needed
-      },
-      // Additional options
-      // fallback: false // Set to true if you want to enable fallback behavior
-    };
-  },
-  basePath: "/TasksTest",
+  output: "export",
 };
 
 export default nextConfig;
