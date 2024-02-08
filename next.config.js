86cc3a7d2c988036ac84a2e1d7581e67da3f5b29
Mozaffar <FERAWWWESS@gmail.com>
2024-02-08 19:17:17 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require("next-compose-plugins");
 
-const basePath = process.env.NODE_ENV === "production" ? "/TasksTest" : "";
-
-const nextConfig = withPlugins([], {
+const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath,
-});
+};
 
 module.exports = nextConfig;
