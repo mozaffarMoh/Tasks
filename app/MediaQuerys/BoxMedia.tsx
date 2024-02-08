dfeef03f851f2c9f0f07b693267c719b1e78d373
Mozaffar <FERAWWWESS@gmail.com>
2024-02-08 17:46:33 +0300

    newday2

--- app/MediaQuerys/BoxMedia.tsx
+++
@@ -1,9 +0,0 @@
-import { useMediaQuery } from "@mui/material";
-
-
-const BoxMediaQuery = () => {
-     const BoxMedia = useMediaQuery("(max-width:800px)");
-     return BoxMedia
-};
-
-export default BoxMediaQuery;
